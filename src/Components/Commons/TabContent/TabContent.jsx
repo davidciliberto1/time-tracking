dd9e923859b2578860d5a25f8d8912f16fd4a315
davidciliberto1 <davidciliberto@gmail.com>
2022-03-04 15:08:50 -0300

    final draft1

--- src/Components/Commons/TabContent/TabContent.jsx
+++ src/Components/Commons/TabContent/TabContent.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import { dataTasks } from '../../../data';
 import Item from '../Item/Item';
 import './tabContent.css';
